2e714c8f233fd34d35163ba94ad7c7932a283380
Pauli Anttila <pauli.anttila@gmail.com>
2024-03-22 17:40:46 +0200

    Fix init

--- code/lib/UseClassificationCNN.py
+++ code/lib/UseClassificationCNN.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 class UseClassificationCNN(CNNBase):
     def __init__(self, in_Modelfile, in_dx, in_dy, in_numberclasses, in_LogImageLocation, in_LogNames):
-        super().__init__(self, in_Modelfile, in_dx, in_dy, in_LogImageLocation, in_LogNames)
+        super().__init__(in_Modelfile, in_dx, in_dy, in_LogImageLocation, in_LogNames)
 
     def ReadoutSingleImage(self, image):
         output_data = super().ReadoutSingleImage(image)
