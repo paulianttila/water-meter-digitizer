31b9cd9d0d68eb6fe5336fef732ff4de5b36885a
Pauli Anttila <pauli.anttila@gmail.com>
2024-04-02 20:56:01 +0300

    Fix config test

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -3,10 +3,10 @@ from src.lib.Config import Config
 
 def test_config():
     config = Config()
-    config.parseConfig("config/config.ini")
+    config.load_config("config/config.ini")
 
-    assert config.httpImageUrl == "file:///config/original.jpg"
-    assert config.httpTimeoutLoadImage == 10
-    assert config.httpImageMinSize == 20000
-    assert config.httpImageLogFolder == "/log/source_image/"
-    assert config.httpLogOnlyFalsePictures is True
+    assert config.http_image_url == "file:///config/original.jpg"
+    assert config.http_load_image_timeout == 10
+    assert config.http_image_min_size == 20000
+    assert config.http_image_log_dir == "/log/source_image/"
+    assert config.http_log_only_false_pictures is True
